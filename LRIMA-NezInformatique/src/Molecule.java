0789bd38161289a7c318d2bd6f94bd74de790f7d
ThomasTrepanier <47257055+ThomasTrepanier@users.noreply.github.com>
2019-04-01 09:46:58 -0400

    StartConverting
    Début de la mise en place des structures de données et de la conversion des formules

---
+++ LRIMA-NezInformatique/src/Molecule.java
@@ -0,0 +1,20 @@
+
+public class Molecule {
+	private String smiles;
+	private float smilesValue;
+
+	public String getSmiles() {
+		return smiles;
+	}
+
+	public void setSmiles(String smiles) {
+		this.smiles = smiles;
+	}
+	public float getSmilesValue() {
+		return this.smilesValue;
+	}
+	
+	public void calculateSmilesValue() {
+		
+	}
+}
