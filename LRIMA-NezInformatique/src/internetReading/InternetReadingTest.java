4dbf9d7211ebb9091aa001cad6116be73bca2c5b
ThomasTrepanier <thomtrepanier@gmail.com>
2019-04-18 19:33:32 -0400

    Revert "Update InternetReadingTest.java"
    This reverts commit 9e5cf67c16666d8994ab69d509c622b4900f7c33.

--- LRIMA-NezInformatique/src/internetReading/InternetReadingTest.java
+++ LRIMA-NezInformatique/src/internetReading/InternetReadingTest.java
@@ -12,22 +12,20 @@ import uk.ac.ebi.beam.Graph;
 
 public class InternetReadingTest {
 
-	//http://www.foo.com/
-	//https://pubchem.ncbi.nlm.nih.gov/compound/180
 	public static void main(String[] args) throws IOException {
 		Graph g = Graph.fromSmiles("CH3-CO-CH3");
 		Graph f = Functions.collapse(g);
 		System.out.println(f.toSmiles());
-		String webPage = "http://www.foo.com/";
+		String webPage = "https://pubchem.ncbi.nlm.nih.gov/compound/180";
         
         String html = Jsoup.connect(webPage).get().html();
         
         System.out.println(html);
         
-//        String htmlBody = "<div id=\"js-rendered-content\"></div> ";
-//        Document doc = Jsoup.parseBodyFragment(htmlBody);
-//        Element body = doc.body();
-//        System.out.println("\n" + body.text());
+        String htmlBody = "<div id=\"js-rendered-content\"></div> ";
+        Document doc = Jsoup.parseBodyFragment(htmlBody);
+        Element body = doc.body();
+        System.out.println("\n" + body.text());
 	}
 
 }
