05a09d15791d27b08b8e1face7b6db8f8d32f965
ThomasTrepanier <47257055+ThomasTrepanier@users.noreply.github.com>
2019-04-23 16:21:54 -0400

    WebCrawlerStart

--- LRIMA-NezInformatique/src/internetReading/InternetReadingTest.java
+++ LRIMA-NezInformatique/src/internetReading/InternetReadingTest.java
@@ -1,31 +1,19 @@
 package internetReading;
 
 import java.io.IOException;
-
+import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import uk.ac.ebi.beam.Functions;
-import uk.ac.ebi.beam.Graph;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class InternetReadingTest {
 
 	public static void main(String[] args) throws IOException {
-		Graph g = Graph.fromSmiles("CH3-CO-CH3");
-		Graph f = Functions.collapse(g);
-		System.out.println(f.toSmiles());
 		String webPage = "https://pubchem.ncbi.nlm.nih.gov/compound/180";
-        
         String html = Jsoup.connect(webPage).get().html();
         
         System.out.println(html);
-        
-        String htmlBody = "<div id=\"js-rendered-content\"></div> ";
-        Document doc = Jsoup.parseBodyFragment(htmlBody);
-        Element body = doc.body();
-        System.out.println("\n" + body.text());
 	}
-
 }
